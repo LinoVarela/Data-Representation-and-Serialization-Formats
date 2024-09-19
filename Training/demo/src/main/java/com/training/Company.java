1d20f72e29e7f5fab22c1acc123bf874d7852be4
LinoVarela <linorafaelcarvalhovarela@gmail.com>
2024-09-19 17:29:45 +0100

    Tutorial
    yes

---
+++ Training/demo/src/main/java/com/training/Company.java
@@ -0,0 +1,38 @@
+package demo.src.main.java.com.training;
+
+
+//import javax.xml.bind.annotation.*;
+
+// @Since 3.0.0
+import jakarta.xml.bind.annotation.*;
+
+import java.util.List;
+
+@XmlRootElement
+@XmlType(propOrder = {"name", "list"})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Company {
+
+  @XmlElement(name = "staff")
+  List<Staff> list;
+
+  String name;
+
+public List<Staff> getList() {
+    return list;
+}
+
+public void setList(List<Staff> list) {
+    this.list = list;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+  // getters, and setters
+}
---
+++ Training/demo/src/main/java/com/training/FruitApp.java
@@ -0,0 +1,77 @@
+package demo.src.main.java.com.training;
+
+/**
+ * Hello world!
+ *
+ */
+
+import com.training.Fruit;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.File;
+
+public class FruitApp {
+
+    public static void main(String[] args) {
+
+        JAXBContext jaxbContext = null;
+        try {
+
+            // Normal JAXB RI
+            //jaxbContext = JAXBContext.newInstance(Fruit.class);
+
+            // EclipseLink MOXy needs jaxb.properties at the same package with Fruit.class
+            // Alternative, I prefer define this via eclipse JAXBContextFactory manually.
+            jaxbContext = org.eclipse.persistence.jaxb.JAXBContextFactory
+                    .createContext(new Class[]{Fruit.class}, null);
+
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+            // output pretty printed
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            Fruit o = new Fruit();
+            o.setId(1);
+            o.setName("Banana");
+            o.setPrice("9.99");
+
+            // output to a xml file
+            jaxbMarshaller.marshal(o, new File("fruit.xml"));
+
+            // output to console
+            jaxbMarshaller.marshal(o, System.out);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+     jaxbContext = null;
+      try {
+
+          // Normal JAXB RI
+          //jaxbContext = JAXBContext.newInstance(Fruit.class);
+
+          // EclipseLink MOXy needs jaxb.properties at the same package with Fruit.class
+          // Alternative, I prefer define this via eclipse JAXBContextFactory manually.
+          jaxbContext = org.eclipse.persistence.jaxb.JAXBContextFactory
+                  .createContext(new Class[]{Fruit.class}, null);
+
+          File file = new File("fruit.xml");
+
+          Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+          Fruit o = (Fruit) jaxbUnmarshaller.unmarshal(file);
+         
+          System.out.println(o.toString());
+
+      } catch (JAXBException e) {
+          e.printStackTrace();
+      }
+
+
+    }
+
+}
